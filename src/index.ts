a0fa76c8712dd36507663048e09883be70bdeba1
osawa-koki <koki@osawa.dev>
2024-09-07 23:54:20 +0900

    Notionデータベースからレコード一覧を取得する。

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,41 @@ function main(_e: GoogleAppsScript.Events.DoPost & Istest) {
   const e = _e ?? mockEventPostData
   const isProd = e.isTest !== true
 
+
+  const notionSecret = properties.getProperty("NOTION_SECRET")!
+  const notionDatabaseId = properties.getProperty("NOTION_DATABASE_ID")!
+
+  const url = `https://api.notion.com/v1/databases/${notionDatabaseId}/query`
+
+  const headers = {
+    "Authorization": `Bearer ${notionSecret}`,
+    "Content-Type": "application/json",
+    "Notion-Version": "2022-06-28"
+  }
+
+  const options = {
+    "method": "post",
+    "headers": headers,
+    "muteHttpExceptions": true
+  }
+
+  const response = UrlFetchApp.fetch(url, options)
+
+  const statusCode = response.getResponseCode()
+  if (statusCode !== 200) {
+    Logger.log(`Error: ${statusCode} - ${response.getContentText()}`)
+    return out.setContent(JSON.stringify({ isProd, message: 'Failed', statusCode, error: response.getContentText() }))
+  }
+
+  const jsonResponse = JSON.parse(response.getContentText())
+
+  const results = jsonResponse.results
+  results.forEach((record) => {
+    const properties = record.properties
+    Logger.log(JSON.stringify(properties, null, 2))
+  })
+
+
   return out.setContent(JSON.stringify({ isProd, properties }))
 }
 
